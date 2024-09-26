8bf3cb4d67fc9d267bcc59aac77ff90bd9d0f5c7
PranavJaju <pranavjaju45@gmail.com>
2024-09-26 22:49:28 +0530

    secretKeyIssue

--- client/action/awsClient.js
+++ client/action/awsClient.js
@@ -2,9 +2,9 @@ import AWS from 'aws-sdk'
 import apiConfig from '../configs/api'
 
 AWS.config.update({
-    accessKeyId: process.env.accessKey,
-    secretAccessKey:  process.env.secretAccessKey,
-    region: 'ap-south-1'  
+    accessKeyId: apiConfig.accessKey,
+    secretAccessKey: apiConfig.secretAccessKey,
+    region: 'ap-south-1'
 })
 
 export default AWS;
--- client/configs/api.js
+++ client/configs/api.js
@@ -1,6 +1,8 @@
 const apiConfig = {
   url: "https://api.submission.pulzion.co.in",
   // url: "http://localhost:3001", 
+  accessKey: process.env.NEXT_PUBLIC_ACCESS_KEY,
+  secretAccessKey: process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY,
 };
 
 export default apiConfig;
